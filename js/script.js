5e8edcc490f617052de41b773f4e7d1f73c1f2a8
giva16 <ltpie1@hotmail.com>
2024-02-15 16:38:21 +0100

    Feature(get-movies): Fetch api gets 20 popular movies from the TMDB API

--- js/script.js
+++ js/script.js
@@ -5,6 +5,29 @@ const global = {
   currentPage: window.location.pathname
 };
 
+// display popular movies on home page
+async function displayPopularMovies() {
+  const results = await fetchAPIData('movie/popular');
+  console.log(typeof results.results);
+}
+
+// Fetch data from TMDB API
+async function fetchAPIData(endpoint) {
+  const API_KEY = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MjUzMjZjNDYzYzNkOWI3NjRhOWNmYTQ0ODdmZjhlOSIsInN1YiI6IjY1Y2QyMmIyYzM5MjY2MDE2MmJmYzM3NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0XW4IkrnSceIn4sIchYY4XX-0ie0hpPVC9HpwlyznhA';
+  const API_URL = 'https://api.themoviedb.org/3/'
+
+  const response = await fetch(`${API_URL}${endpoint}?language=en-US`, {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${API_KEY}`
+    }
+  });
+
+  const data = await response.json();
+
+  return data;
+}
 
 // highlight active link
 function highlightActiveLink() {
@@ -41,6 +64,7 @@ function init() {
 
   }
   highlightActiveLink();
+  displayPopularMovies();
 }
 
 
